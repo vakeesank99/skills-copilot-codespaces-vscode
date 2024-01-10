3215c6f7128fe3a7dedc572b25962ebed91025ef
Vakeesan Karunanithy <76678677+vakeesank99@users.noreply.github.com>
2024-01-10 16:19:20 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,72 +1,45 @@
 //create webserver
-
+var express = require('express');
 var router = express.Router();
-var Comment = require('../models/comment');
-var Post = require('../models/post');
-var User = require('../models/user');
-var mongoose = require('mongoose');
-var passport = require('passport');
-var auth = require('../config/auth');
+var db = require('../db');
 
 //get all comments
-router.get('/', function(req, res) {
-  Comment.find(function(err, comments) {
-    if(err) {
-      res.send(err);
-    }
-    res.json(comments);
+router.get('/', function(req, res, next) {
+  db.query('SELECT * FROM comments', function (error, results, fields) {
+    if (error) throw error;
+    res.send(JSON.stringify(results));
   });
 });
 
-//add comment
-router.post('/', auth.isAuthenticated, function(req, res) {
-  var comment = new Comment();
-  comment.text = req.body.text;
-  comment.post = req.body.post;
-  comment.user = req.user._id;
-  comment.save(function(err, comment) {
-    if(err) {
-      res.send(err);
-    }
-    res.json(comment);
+//get comments by id
+router.get('/:id', function(req, res, next) {
+  db.query('SELECT * FROM comments WHERE id = ?', [req.params.id], function (error, results, fields) {
+    if (error) throw error;
+    res.send(JSON.stringify(results));
   });
 });
 
-//get comment by id
-router.get('/:id', function(req, res) {
-  Comment.findById(req.params.id, function(err, comment) {
-    if(err) {
-      res.send(err);
-    }
-    res.json(comment);
+//create new comment
+router.post('/', function(req, res, next) {
+  db.query('INSERT INTO comments (id, comment, user_id, post_id) VALUES (?, ?, ?, ?)', [req.body.id, req.body.comment, req.body.user_id, req.body.post_id], function (error, results, fields) {
+    if (error) throw error;
+    res.send(JSON.stringify(results));
   });
 });
 
 //update comment
-router.put('/:id', auth.isAuthenticated, function(req, res) {
-  Comment.findById(req.params.id, function(err, comment) {
-    if(err) {
-      res.send(err);
-    }
-    comment.text = req.body.text;
-    comment.post = req.body.post;
-    comment.user = req.user._id;
-    comment.save(function(err, comment) {
-      if(err) {
-        res.send(err);
-      }
-      res.json(comment);
-    });
+router.put('/:id', function(req, res, next) {
+  db.query('UPDATE comments SET comment = ?, user_id = ?, post_id = ? WHERE id = ?', [req.body.comment, req.body.user_id, req.body.post_id, req.params.id], function (error, results, fields) {
+    if (error) throw error;
+    res.send(JSON.stringify(results));
   });
 });
 
 //delete comment
-router.delete('/:id', auth.isAuthenticated, function(req, res) {
-  Comment.findByIdAndRemove(req.params.id, function(err, comment) {
-    if(err) {
-      res.send(err);
-    }
-    res.json(comment);
+router.delete('/:id', function(req, res, next) {
+  db.query('DELETE FROM comments WHERE id = ?', [req.params.id], function (error, results, fields) {
+    if (error) throw error;
+    res.send(JSON.stringify(results));
   });
 });
 
